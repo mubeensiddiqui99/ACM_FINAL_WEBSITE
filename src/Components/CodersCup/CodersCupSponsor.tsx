db16cb18a96afd00824818f2e46f34c5cdf3049e
samuzaffar99 <sam.muzaffar99@gmail.com>
2021-10-26 13:18:22 +0500

    rename typo in sponsor

--- src/Components/CodersCup/CodersCupSponsor.tsx
+++ src/Components/CodersCup/CodersCupSponsor.tsx
@@ -4,11 +4,11 @@ const CodersCupSponsor = () => {
   return (
     <>
       <div style={{ marginBottom: "20%" }}>
-        <h1 className="sponser">OUR SPONSOR</h1>
+        <h1 className="sponsor">OUR SPONSOR</h1>
         <div className="img1">
-          <img src="" alt="sponser1" style={{ marginLeft: "20px" }} />
-          <img src="" alt="sponser2" style={{ marginLeft: "20px" }} />
-          <img src="" alt="sponser3" style={{ marginLeft: "20px" }} />
+          <img src="" alt="sponsor1" style={{ marginLeft: "20px" }} />
+          <img src="" alt="sponsor2" style={{ marginLeft: "20px" }} />
+          <img src="" alt="sponsor3" style={{ marginLeft: "20px" }} />
         </div>
       </div>
     </>
